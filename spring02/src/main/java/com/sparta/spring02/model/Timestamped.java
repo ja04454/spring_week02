13c9670456a764c85692d9792003087a9ba749c5
ja04454 <ja04454@gmail.com>
2022-06-17 18:03:34 +0900

    [Model] Timestamped 작성

---
+++ spring02/src/main/java/com/sparta/spring02/model/Timestamped.java
@@ -0,0 +1,19 @@
+package com.sparta.spring02.model;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import java.time.LocalDateTime;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+}
